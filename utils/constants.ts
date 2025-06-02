116861d870409943365ca2985000d73eb0c089dc
Syed Dharwesh S <162027365+zyedd07@users.noreply.github.com>
2025-06-02 21:34:49 +0530

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,16 @@
+// src/utils/constants.ts
+
 import { CourseTeacherServiceOperationType } from "./types"
 
 export const Role = {
     ADMIN: 'admin',
     TEACHER: 'teacher',
     STUDENT: 'student',
-}
+} as const; 
+
+
+export type RoleValue = typeof Role[keyof typeof Role];
+
 
 export const CourseTeacherServiceOperation: { ASSIGN: CourseTeacherServiceOperationType, UNASSIGN: CourseTeacherServiceOperationType } = {
     ASSIGN: 'assign',
