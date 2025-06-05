7d02a0034e26cb9d213a68718d29da28b309a4e7
Syed Dharwesh S <162027365+zyedd07@users.noreply.github.com>
2025-06-05 13:34:30 +0530

    Update Course.router.ts

--- routes/Course.router.ts
+++ routes/Course.router.ts
@@ -1,15 +1,20 @@
 import express from 'express';
-import * as CourseController from '../controllers/Course.controller';
-import isAuth from '../middleware/auth';
+import * as CourseController from '../controllers/Course.controller'; // Import all controllers
+import isAuth from '../middleware/auth'; // Import your authentication middleware
 
 const router = express.Router();
 
 router.post('/create', isAuth, CourseController.createCourseController);
-router.get('/', isAuth, CourseController.getCoursesController);
+router.get('/', isAuth, CourseController.getCoursesController); // Can be used for general listing/filtering
+router.get('/:id', isAuth, CourseController.getCourseByIdController); // Specific course by ID
 router.put('/:id', isAuth, CourseController.updateCourseController);
 router.delete('/:id', isAuth, CourseController.deleteCourseController);
+
 router.post('/teacher', isAuth, CourseController.courseTeacherController);
 router.get('/getAssignedCourses', isAuth, CourseController.getAssignedCourseController);
-router.get('/:id', isAuth, CourseController.getCourseByIdController); 
+
+router.post('/:courseId/contents', isAuth, CourseController.addCourseContentController);
+router.put('/:courseId/contents/:contentId', isAuth, CourseController.updateCourseContentController);
+router.delete('/:courseId/contents/:contentId', isAuth, CourseController.deleteCourseContentController);
 
 export default router;
