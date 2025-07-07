49bb8c4b4dda14650eee00b1b23581a8fe6aac11
Syed Dharwesh S <162027365+zyedd07@users.noreply.github.com>
2025-07-07 19:08:33 +0000

    teache

--- routes/teacher.router.ts
+++ routes/teacher.router.ts
@@ -7,10 +7,10 @@ const router = express.Router();
 // This assumes you might have other teacher-specific routes in the future.
 
 // GET a specific teacher's page permissions
-router.get('/:userId/permissions', isAuth, authorizeAdmin, TeacherController.getTeacherPermissions);
+router.get('/:teacherId/permissions', isAuth, authorizeAdmin, TeacherController.getTeacherPermissions);
 
 // PUT (update) a specific teacher's page permissions
-router.put('/:userId/permissions', isAuth, authorizeAdmin, TeacherController.updateTeacherPermissions);
+router.put('/:teacherId/permissions', isAuth, authorizeAdmin, TeacherController.updateTeacherPermissions);
 
 
 export default router;
