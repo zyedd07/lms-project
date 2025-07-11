676c9d0574bbdaa7bc84b0131919df2f636beee1
Syed Dharwesh S <162027365+zyedd07@users.noreply.github.com>
2025-07-11 16:32:51 +0530

    Update UserTestSeries.router.ts

--- routes/UserTestSeries.router.ts
+++ routes/UserTestSeries.router.ts
@@ -5,13 +5,13 @@ import isAuth, { authorizeAdmin } from '../middleware/auth';
 const router = express.Router();
 
 // Enroll a user in a test series
-router.post('/enroll', isAuth, authorizeAdmin, UserTestSeriesController.enrollInTestSeries);
+router.post('/enroll', isAuth, UserTestSeriesController.enrollInTestSeries);
 
 // Get all test series for a specific user
-router.get('/user/:userId/testseries', isAuth, authorizeAdmin, UserTestSeriesController.getUserTestSeries);
+router.get('/user/:userId/testseries', isAuth, UserTestSeriesController.getUserTestSeries);
 
 // Unenroll a user from a test series
-router.delete('/unenroll', isAuth, authorizeAdmin, UserTestSeriesController.unenrollFromTestSeries);
+router.delete('/unenroll', isAuth, UserTestSeriesController.unenrollFromTestSeries);
 
 // Update the status of a test series enrollment
 router.put('/status', isAuth, authorizeAdmin, UserTestSeriesController.updateEnrollmentStatus);
