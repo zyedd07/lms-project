174c7c75eae45fe513706da9bb000718fff48de2
Syed Dharwesh S <162027365+zyedd07@users.noreply.github.com>
2025-07-11 16:32:36 +0530

    Update UserQbank.router.ts

--- routes/UserQbank.router.ts
+++ routes/UserQbank.router.ts
@@ -5,13 +5,13 @@ import isAuth, { authorizeAdmin } from '../middleware/auth';
 const router = express.Router();
 
 // Enroll a user in a Q-Bank
-router.post('/enroll', isAuth, authorizeAdmin, UserQbankController.enrollInQbank);
+router.post('/enroll', isAuth, UserQbankController.enrollInQbank);
 
 // Get all Q-Banks for a specific user
-router.get('/user/:userId/qbanks', isAuth, authorizeAdmin, UserQbankController.getUserQbanks);
+router.get('/user/:userId/qbanks', isAuth, UserQbankController.getUserQbanks);
 
 // Unenroll a user from a Q-Bank
-router.delete('/unenroll', isAuth, authorizeAdmin, UserQbankController.unenrollFromQbank);
+router.delete('/unenroll', isAuth, UserQbankController.unenrollFromQbank);
 
 // Update the status of a Q-Bank enrollment
 router.put('/status', isAuth, authorizeAdmin, UserQbankController.updateEnrollmentStatus);
